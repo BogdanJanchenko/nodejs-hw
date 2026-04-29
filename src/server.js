dc780ca0d2d3fbcd4ff0d2c545d8602e673abdfa
Bodya Janchenko <robotosoid2000@gmail.com>
2026-04-29 19:12:23 +0300

    fix middlewar time

--- src/server.js
+++ src/server.js
@@ -25,10 +25,10 @@ app.use(
   }),
 );
 
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString}`);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString}`);
+//   next();
+// });
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
