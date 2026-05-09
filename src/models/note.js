93337e315600b9e33f674ace333aefcdc6dc87ae
Bodya Janchenko <robotosoid2000@gmail.com>
2026-05-09 20:07:30 +0300

    full code

---
+++ src/models/note.js
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+
+const notesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    tag: {
+      type: String,
+      required: false,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Notes = model('Note', notesSchema);
