f948d7e7610a0ccc482843f215dc4cbe77dd8156
Bodya Janchenko <robotosoid2000@gmail.com>
2026-05-15 18:00:59 +0300

    .required() fix

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -34,7 +34,7 @@ export const updateNoteSchema = {
 
 export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1),
+    title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
