d4bcceb67dda5f3318b73733bf39b8e274524373
Bodya Janchenko <robotosoid2000@gmail.com>
2026-05-25 11:41:34 +0300

    fix sessions

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookie } from '../services/auth.js';
-import { Session } from '../models/sessions.js';
+import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
