5cfe3eb4e391bdc864e2df1d5e39b6c24952014c
Bodya Janchenko <robotosoid2000@gmail.com>
2026-05-30 20:33:26 +0300

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendMail } from '../utils/sendMail.js';
 
 import path from 'node:path';
 import fs from 'node:fs/promises';
@@ -123,7 +123,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendEmail({
+    await sendMail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
